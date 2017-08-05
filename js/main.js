d1f04cc663b2055365b4b801429296a486ad1ceb
pilgrim011 <printerstampac@gmail.com>
2017-08-05 17:43:38 +0200

    17

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ jQuery(function($) {
 
 	//smooth scroll
 	$('.navbar-nav > li').click(function(event) {
-		event.preventDefault();
+		//event.preventDefault();
 		var target = $(this).find('>a').prop('hash');
 		$('html, body').animate({
 			scrollTop: $(target).offset().top
@@ -52,5 +52,5 @@ jQuery(function($) {
 			return false;
 		});
 	});*/
-	
+
 });
